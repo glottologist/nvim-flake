33d478f6b5216f3e166e4ccfd8c9193e3428d5dd
jason <jason@ridgway-taylor.co.uk>
2024-04-28 15:30:10 +0800

    fix: pinned keylog to specific rev

--- flake.nix
+++ flake.nix
@@ -399,7 +399,7 @@
     };
 
     keylog-nvim = {
-      url = "github:glottologist/keylog.nvim#b75c21f9307f5070311dc295ac202c51d3bc1d4e";
+      url = "github:glottologist/keylog.nvim/b75c21f9307f5070311dc295ac202c51d3bc1d4e";
       flake = false;
     };
     cheatsheet-nvim = {
